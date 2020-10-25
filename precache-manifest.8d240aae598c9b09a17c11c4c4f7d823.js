c60b803e934084b0d10b1282c3b24210dfdea9de
AleMiranda16 <damm.alejandra@hotmail.com>
2020-10-24 22:24:05 -0700

    Updates

---
+++ precache-manifest.8d240aae598c9b09a17c11c4c4f7d823.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07aa645345cb34ab0acfbc4eb1fc2d68",
+    "url": "/Practica1React/index.html"
+  },
+  {
+    "revision": "1c260959165d7921d9ba",
+    "url": "/Practica1React/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "a4f31de9d39c95510076",
+    "url": "/Practica1React/static/css/main.d0036824.chunk.css"
+  },
+  {
+    "revision": "1c260959165d7921d9ba",
+    "url": "/Practica1React/static/js/2.021dcc47.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/Practica1React/static/js/2.021dcc47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4f31de9d39c95510076",
+    "url": "/Practica1React/static/js/main.03882204.chunk.js"
+  },
+  {
+    "revision": "90b61aaee6f7aa1825dd",
+    "url": "/Practica1React/static/js/runtime-main.da9b2c16.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Practica1React/static/media/logo.5d5d9eef.svg"
+  }
+]);
